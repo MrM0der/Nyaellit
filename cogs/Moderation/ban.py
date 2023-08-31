0a5cf513752102261a3d2dbb2e29496124eb8ce7
Orzxiety <83078982+Orzxiety@users.noreply.github.com>
2023-08-31 18:14:03 +0500

    modified:   cogs/Moderation/ban.py

--- cogs/Moderation/ban.py
+++ cogs/Moderation/ban.py
@@ -5,5 +5,9 @@ class Ban(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.command(name='ban')
+    async def _ban(self, ctx):
+        await ctx.send('иди нахуй')
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(Ban(bot))
